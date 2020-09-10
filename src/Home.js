8a9b7c51b6d58b576f30239feacc48b67f6d16d4
rafa2701 <leonr2701@gmail.com>
2020-09-10 18:57:01 -0400

    Full Day 2 Javascript Challenge

---
+++ src/Home.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div>
+      <div className="home">
+        <div className="home__container">
+          <img
+            className="home__image"
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+            alt="Hero Banner"
+          />
+
+          <div className="home__row">
+            <Product
+              title="GTA V PC Game"
+              price={29.99}
+              image="https://images-na.ssl-images-amazon.com/images/I/916T5H6sCtL._AC_SL1500_.jpg"
+              rating={5}
+            />
+            <Product
+              id="49538094"
+              title="Nintendo Switch Neon Edition 32 GB Console"
+              price={399.0}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/31rTJsXrJqL.jpg"
+            />
+          </div>
+
+          <div className="home__row">
+            <Product
+              id="49558094"
+              title="Google Pixel 4A - US Model - GSM Unlocked- 64GB"
+              price={199.0}
+              rating={4}
+              image="https://m.media-amazon.com/images/I/61C9GrXEp4L._AC_SS350_.jpg"
+            />
+            <Product
+              id="49558194"
+              title="7-in-1 Electric Pressure Cooker, 6 Quart, 14 One-Touch Programs"
+              price={79.0}
+              rating={3}
+              image="https://images-na.ssl-images-amazon.com/images/I/81i8c1fSkyL._AC_SL1500_.jpg"
+            />
+            <Product
+              id="49558094"
+              title="Apple AirPods with Charging Case (Wired)"
+              price={89.0}
+              rating={5}
+              image="https://images-na.ssl-images-amazon.com/images/I/71NTi82uBEL._AC_SL1500_.jpg"
+            />
+          </div>
+
+          <div className="home__row">
+            {/*Product */}
+            <Product
+              id="49558094"
+              title="Sceptre Curved 27'' 75Hz LED Monitor HDMI VGA Build-In Speakers, EDGE-LESS Metal Black 2019 (C275W-1920RN)"
+              price={189.0}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/91C9kmKqlkL._AC_SL1500_.jpg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBq_oSqumMUSj00Yhh7wz12TMFnGtOe1kA",
+    authDomain: "challenge-a252a.firebaseapp.com",
+    databaseURL: "https://challenge-a252a.firebaseio.com",
+    projectId: "challenge-a252a",
+    storageBucket: "challenge-a252a.appspot.com",
+    messagingSenderId: "796980281012",
+    appId: "1:796980281012:web:09fc75dc82440a3d98a1e1",
+    measurementId: "G-VZSSJMVRW7"
+  };
