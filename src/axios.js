84dcc708bd06a8e1cb2b3cf7caaacf4377c42b8c
rafa2701 <leonr2701@gmail.com>
2020-09-16 12:27:34 -0400

    Full App (Cloud API yet to Solve)

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create ({
-    baseURL: '' 
+    baseURL: 'https://us-central1-challenge-a252a.cloudfunctions.net/api' 
     // API local cloud function url http://localhost:5001/challenge-a252a/us-central1/api
 });
 
